ca14d0974ce45e02fc3c7c45cc5775fd8fc6a262
Drake Petersen <petersendrake@gmail.com>
2019-03-03 23:35:52 -0500

    WIP local and remote network connections

--- volatility/plugins/freebsd/net_conns.py
+++ volatility/plugins/freebsd/net_conns.py
@@ -37,7 +37,7 @@ class Net_Conns(interfaces_plugins.PluginInterface):
                     description = "Memory layer for the kernel",
                     architectures = ['Intel32', 'Intel64']),
                 requirements.SymbolTableRequirement(
-                   name = 'freebsd',  #TODO: is this right?
+                   name = 'freebsd',
                    description = "FreeBSD kernel Symbols")
             ]
 
@@ -62,7 +62,20 @@ class Net_Conns(interfaces_plugins.PluginInterface):
                    filter: Callable[[int],bool] = lambda _:False) \
                    -> Iterable[interfaces.objects.ObjectInterface]:
         """List all connections in primary layer"""
-        pass
+        view = contexts.Module(context,
+                            freebsd_symbols,
+                            layer_name,
+                            0,
+                            absolute_symbol_addresses=True)
+        #probably going to need other symbols too
+        ripcbinfo = view.object(symbol_name = "ripcbinfo").cast("inpcbinfo")
+        inpcb = ripcbinfo.ipi_listhead.dereference().lh_first.dereference()
+        
+        while inpcb is not None and inpcb.vol.offset != 0:
+            yield inpcb.cast("inpcb")
+            inpcb = inpcb.inp_list.le_next.dereference()
+
+
 
     def _generator(self):
         """Produces all connections after filtering"""
@@ -72,12 +85,18 @@ class Net_Conns(interfaces_plugins.PluginInterface):
                 self.config['freebsd'],
                 #TODO: change pid
                 filter = self.create_filter([self.config.get('pid', None)])):
-            pass
+            proto = 'TEST'
+            l_host = conn.inp_inc.inc_ie.ie_dependladdr.ie46_local.ia46_addr4.s_addr
+            l_port = conn.inp_inc.inc_ie.ie_lport
+            r_host = conn.inp_inc.inc_ie.ie_dependfaddr.ie46_foreign.ia46_addr4.s_addr
+            r_port = conn.inp_inc.inc_ie.ie_fport
+            yield (0,(proto, l_host, l_port, r_host, r_port))
+
 
     def run(self):
         """Entry point for plugin"""
         #TODO: PPID of processes that own the connection?
         return renderers.TreeGrid(
-                [("Protocol", str), ("Local IP", int), ("Local Port", int),
-                    ("Remote IP", int),("Remote Port", int)], 
+                [("PROT", str), ("L_IP", int), ("LPORT", int),
+                    ("R_IP", int),("RPORT", int)], 
                 self._generator())
